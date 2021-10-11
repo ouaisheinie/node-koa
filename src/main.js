3ce4950ae7678317b551d0a776f3345c3a0bb54b
zhangzeyi <zhangzeyi@starlinke.com>
2021-10-11 15:52:48 +0800

    -u

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
-const Koa = require('koa')
-
 const { APP_PORT } = require('./config/config.default')
-
-const app = new Koa()
-
-const userRouter = require('./router/user.route')
-app.use(userRouter.routes())
-
+const app = require('./app/index')
 
 app.listen(APP_PORT, () => {
 	console.log('server is running on localhost:' + APP_PORT)
--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,9 +1,12 @@
 const Router = require('koa-router')
+const { register, login } = require('../controller/user.controller')
 
-const router = new Router({ prefix: '/users' })
+const router = new Router({ prefix: '/users' }) // api前缀
 
-router.get('/', (ctx, next) => {
-  ctx.body = 'hello users'
-})
+// 用户注册接口
+router.post('/register', register) // /users/register 这加上了前缀
+
+// 用户登录接口
+router.post('/login', login)
 
 module.exports = router
