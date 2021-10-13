c67bc58b25ecfdcf2a1799454578d0b43444951d
zhangzeyi <zhangzeyi@starlinke.com>
2021-10-13 16:33:38 +0800

    09 数据库操作

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,13 +3,20 @@ const { createUser } = require('../service/user.service')
 class UserController {
   async register(ctx, next) {
     // 1.获取数据
-    console.log(ctx.request.body)
+    // console.log(ctx.request.body)
     const { user_name, password } = ctx.request.body
     // 2.操作数据库 大型项目会有单独的曾 抽出来做数据库操作 此项目放在service里
     const res = await createUser(user_name, password)
     // console.log(res)
     // 3.返回结果
-    ctx.body = ctx.request.body
+    ctx.body = {
+      code: 0,
+      message: '用户注册成功',
+      result: {
+        id: res.id,
+        user_name: res.user_name
+      }
+    }
   }
 
   async login(ctx, body) {
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,8 +1,12 @@
-// 一个操作集合的类 service 这一层主要是操作数据库
+// 一个操作数据库的类 service 这一层主要是操作数据库
+const User = require('../model/user.model')
+
 class UserService {
   async createUser(user_name, password) {
-    // todo: 写入数据库
-    return '写入数据库成功'
+    // 往数据库插入数据
+    const res = await User.create({ user_name, password })
+    // console.log(res)
+    return res.dataValues
   }
 }
 
