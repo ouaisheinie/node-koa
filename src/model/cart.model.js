47180641ce5432b110fb91e07b824a0f22331f97
ouaisheinie <346114608@qq.com>
2021-11-17 00:45:25 +0800

    31 添加购物车 下

--- src/model/cart.model.js
+++ src/model/cart.model.js
@@ -1,4 +1,37 @@
 // 操作数据库
+// 1. 导入sequelize
 const { DataTypes } = require('sequelize')
 
+// 连接数据库
 const seq = require('../db/seq')
+
+const Cart = seq.define('zy_carts', {
+  goods_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    comment: '商品id'
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    comment: '用户id'
+  },
+  number: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 1,
+    comment: '商品数量'
+  },
+  selected: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+    comment: '是否选中'
+  }
+})
+
+// Cart.sync({
+//   force: true
+// })
+
+module.exports = Cart
--- src/service/cart.service.js
+++ src/service/cart.service.js
@@ -1,14 +1,28 @@
 // 操作购物车数据库表的类
 const Cart = require('../model/cart.model')
+const { Op } = require('sequelize')
 
 class CartService {
   async createOrUpdate(user_id, goods_id) {
-    return {
-      id: 1,
-      user_id: 13,
-      goods_id: 1,
-      number: 1,
-      selected: true
+    // 根据user_id、goods_id同时去查找
+    let res = await Cart.findOne({
+      where: {
+        [Op.and]: {
+          user_id,
+          goods_id
+        }
+      }
+    })
+
+    if (res) { // res不为空 说明已经存在记录 于是把记录的number + 1
+      res.increment('number') // += 1
+      return await res.reload() // 更新以后 要调用reload 更新数据库
+    } else { // res 为空 新建一条记录 然后返回
+      return await Cart.create({
+        user_id,
+        goods_id,
+        // number selected 有默认值
+      })
     }
   }
 }
